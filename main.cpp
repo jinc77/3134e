f37b0eb42d879079189f453c6581939e86d91404
NJvK <vonkrogh.natalie@gmail.com>
2025-01-22 18:37:11 -0800

    changed auton a little

--- main.cpp
+++ main.cpp
@@ -119,53 +119,53 @@ void TurnInertial(int Degrees){
 
 void autonomous() {
     // Set brake mode to hold
-    DrivetrainInertial.reset();
-    TurnInertial(90);
-    // RightDriveSmart.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-    // LeftDriveSmart.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-    // Intake.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-    // HighStakes.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    //DrivetrainInertial.reset();
+    //TurnInertial(90);
+    RightDriveSmart.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    LeftDriveSmart.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    Intake.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    HighStakes.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 
-    // ToggleClamp(); //the clamp starts at true then moves to false
-    // pros::delay(500);
+    ToggleClamp(); //the clamp starts at true then moves to false
+    pros::delay(500);
 
-    // HighStakes.move_velocity(-50);
-    // pros::delay(500);
-    // HighStakes.move_velocity(0);
-    // // Move motors at specified velocities
-    // RightDriveSmart.move_velocity(-50);
-    // LeftDriveSmart.move_velocity(50); // drives in reverse
+    HighStakes.move_velocity(-50);
+    pros::delay(500);
+    HighStakes.move_velocity(0);
+    // Move motors at specified velocities
+    RightDriveSmart.move_velocity(-50);
+    LeftDriveSmart.move_velocity(50); // drives in reverse
 
-    // // Delay for 1 second
-    // pros::delay(3000);
+    // Delay for 1 second
+    pros::delay(3000);
 
-    // // Stop motors
-    // RightDriveSmart.move_velocity(0);
-    // LeftDriveSmart.move_velocity(0);
-    // pros::delay(500);
+    // Stop motors
+    RightDriveSmart.move_velocity(0);
+    LeftDriveSmart.move_velocity(0);
+    pros::delay(500);
 
-    // ToggleClamp(); //the clamp is now at false then moves to true
-    // pros::delay(500);
+    ToggleClamp(); //the clamp is now at false then moves to true
+    pros::delay(500);
     
-    // Intake.move_velocity(200);
+    Intake.move_velocity(200);
 
-    // pros::delay(700);
+    pros::delay(700);
 
-    // Intake.move_velocity(0);
+    Intake.move_velocity(0);
 
-    // pros::delay(500);
+    pros::delay(500);
 
-    // RightDriveSmart.move_velocity(-50);
-    // LeftDriveSmart.move_velocity(50); // drives reverse
+    RightDriveSmart.move_velocity(-50);
+    LeftDriveSmart.move_velocity(50); // drives reverse
 
-    // pros::delay(500);
+    pros::delay(500);
 
-    // RightDriveSmart.move_velocity(0);
-    // LeftDriveSmart.move_velocity(0);
+    RightDriveSmart.move_velocity(0);
+    LeftDriveSmart.move_velocity(0);
 
-    // HighStakes.move_velocity(50);
-    // pros::delay(500);
-    // HighStakes.move_velocity(0);
+    HighStakes.move_velocity(50);
+    pros::delay(500);
+    HighStakes.move_velocity(0);
 }
 
 /**
